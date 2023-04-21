9804da7d9f7793fdde40bde1e32ab4b369de14e2
HRS8 <harshasai3789@gmail.com>
2023-04-21 23:11:51 +0530

    defined layout

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -36,14 +36,19 @@ const Quiz = () => {
   };
 
   return (
-    <div className=" flex justify-center items-center align-middle  h-screen ">
+    <div className=" flex  align-middle   flex-col  items-center h-screen ">
       <div
-        className="border-2 border-white  p-6 rounded-md m-auto sm:w-[50vw]"
+        className="border-2 border-white  p-6 rounded-md m-auto mb-0 sm:w-[50vw]"
         key={Questions[currentQuestionIndex].id}
       >
-        <h2 className="vertical-center m-2">
-          <span className="border-2 border-white rounded-lg p-1">{Questions[currentQuestionIndex].id}</span>&nbsp;&nbsp;&nbsp;
-          {Questions[currentQuestionIndex].Ques}
+        <h2 className="vertical-center m-2 mb-6">
+          <span className="border-2 border-white  vertical-top px-2 py-[0.12rem] rounded-full text-[0.9rem] font-bold">
+            {Questions[currentQuestionIndex].id}
+          </span>
+          &nbsp;&nbsp;&nbsp;
+          <span className="text-2xl font-bold">
+            {Questions[currentQuestionIndex].Ques}
+          </span>
         </h2>
         <div className="">
           {Questions[currentQuestionIndex].options.map((option, index) => (
@@ -52,33 +57,40 @@ const Quiz = () => {
               onClick={(event) =>
                 handleOptions(Questions[currentQuestionIndex].id, index, event)
               }
-              className="ml-10"
+              className="pl-4 mb-5 border-2 border-white rounded-xl py-2 text-lg"
             >
               {option}
             </p>
           ))}
         </div>
-        <div key={Questions[currentQuestionIndex]}>
-          <button
-            onClick={() => handleToggle(Questions[currentQuestionIndex].id)}
-          >
-            {isVisible ? "Hide" : "show"} Para
-          </button>
-          {isVisible === Questions[currentQuestionIndex].id && (
-            <p>
-              {Questions[currentQuestionIndex].ans}
-              {isVisible ? "hide" : "show"}
-            </p>
-          )}
-        </div>
         <div key={Questions[currentQuestionIndex].id}>
           <button
             onClick={handleNext}
             disabled={currentQuestionIndex === Questions.length - 1}
+            className="font-bold mt-3"
           >
             Next
           </button>
         </div>
+      </div>
+      <div
+        key={Questions[currentQuestionIndex]}
+        className={`${
+          isVisible ? "border-2 border-white" : ""
+        } m-auto mt-3 sm:w-[50vw] p-3 rounded-md`}
+      >
+        <button
+          onClick={() => handleToggle(Questions[currentQuestionIndex].id)}
+          className="border-none border-0"
+        >
+          {isVisible ? "Hide" : "show"} Para
+        </button>
+        {isVisible === Questions[currentQuestionIndex].id && (
+          <p className="p-3">
+            {Questions[currentQuestionIndex].ans}
+            {isVisible ? "hide" : "show"}
+          </p>
+        )}
       </div>
     </div>
   );
